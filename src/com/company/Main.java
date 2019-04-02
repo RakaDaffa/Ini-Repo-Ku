f187cf72a200b6f66989bd5dfbcce3553bb382b0
Raka-6706174064 <48679786+Raka-6706174064@users.noreply.github.com>
2019-04-02 09:21:45 +0700

    Revers

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,23 +4,35 @@ import java.util.Scanner;
 
 public class Main {
 
-    static int n1=1,n2=1,n3=0,n4=2;
-
-    static void printFibonacci(int count){
-        if(count>0){
-            n3 = n1 + n2;
-            n1 = n2;
-            n2 = n3;
-            n4 = n4+n3;
-            printFibonacci(count-1);
-        }
-    }
+    static void reverseFibonacci(int n) 
+    { 
+        int a[] = new int[n]; 
+      
+        // assigning first and second elements 
+        a[0] = 1; 
+        a[1] = 1; 
+      
+        for (int i = 2; i < n; i++) 
+        { 
+      
+            // storing sum in the 
+            // preceding location 
+            a[i] = a[i - 2] + a[i - 1]; 
+        } 
+      
+        for (int i = n - 1; i >= 0; i--)  
+        { 
+      
+            // printing array in 
+            // reverse order 
+            System.out.print(a[i] +" "); 
+        } 
+    } 
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Masukan Angka : ");
         int count = in.nextInt();
-        printFibonacci(count-2);
-        System.out.print(n4);
+        reverseFibonacci(count);
     }
 }
