b0c5a0aeef640f1fc614ee44fc8753093239e8a2
RakaDaffa <32626581+RakaDaffa@users.noreply.github.com>
2019-04-02 09:01:29 +0700

    Update Jadi 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 
 public class Main {
 
-    static int n1=1,n2=1,n3=0;
+    static int n1=1,n2=1,n3=0,n4=2;
 
     static void printFibonacci(int count){
         if(count>0){
             n3 = n1 + n2;
             n1 = n2;
             n2 = n3;
-            System.out.print(" "+n3);
+            n4 = n4+n3;
             printFibonacci(count-1);
         }
     }
@@ -20,7 +20,7 @@ public class Main {
         Scanner in = new Scanner(System.in);
         System.out.print("Masukan Angka : ");
         int count = in.nextInt();
-        System.out.print(n1+" "+n2);
         printFibonacci(count-2);
+        System.out.print(n4);
     }
 }
